8b65aeba0e90030f58f6562200c9cb1c2c82dfef
luckey_coder <ujumuah@gmail.com>
2021-09-28 21:36:38 +0100

    HomePage done

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -2,8 +2,7 @@ import React from "react"
 import Expenses from './expenseslist'
  const a =()=>(
      <div>
-        <h1>I am jumauh uthman A reactive developer</h1>
-        <h2>This are my expense of the week!</h2>
+         <h2>This are my expense of the week!</h2>
         <Expenses/>
      </div>
  );
